95651270503838eff1b6968266519fe424793f53
CarrumDraws <calumchansy@gmail.com>
2023-09-08 22:47:32 -0700

    Added editProfile Button

--- src/scenes/homePage/index.js
+++ src/scenes/homePage/index.js
@@ -2,7 +2,10 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setLogout } from "../../state";
 import RefreshData from "../../widgets/RefreshData";
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const chan_id = useSelector((state) => state.chan_id);
   const chan_token = useSelector((state) => state.chan_token);
@@ -15,6 +18,7 @@ function Home() {
   return (
     <div>
       <button onClick={() => dispatch(setLogout())}>Sign Out</button>
+      <button onClick={() => navigate("/editprofile")}>Edit Profile</button>
       Home
     </div>
   );
